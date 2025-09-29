647800494b1b6b93bbfe0824d1e06b9a2b7c1a8f
dylan dantonio <dylan.dantonio@ASTIDOM.com>
2025-09-29 15:28:54 +0200

    feat(dashboard): stylisation de la page dashboard pour répondre au cahier des charges.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,59 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { NgApexchartsModule } from "ng-apexcharts";
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+import { NgApexchartsModule } from 'ng-apexcharts';
 import { Router } from '@angular/router';
 import { PieChartOptions } from 'src/app/core/types/PieChartOptions';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [NgApexchartsModule, HttpClientModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
   public chartOptions: PieChartOptions | undefined;
+  public numberCountries: number = 0;
+  public numberJOs: number = 0;
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
-    this.http.get<Olympic[]>('assets/mock/olympic.json').subscribe(data => {
-      const labels: string[] = [];
+    this.http.get<Olympic[]>('assets/mock/olympic.json').subscribe((data) => {
+      let labels: string[] = [];
       const series: number[] = [];
-      data.forEach(country => {
-        labels.push(country.country);
+      const ids: Number[] = [];
+
+      data.forEach((country) => {
         const totalMedals = country.participations.reduce(
-          (sum: number, participation: any) => sum + participation.medalsCount, 0
+          (sum: number, participation: any) => sum + participation.medalsCount,
+          0
         );
+        labels.push(country.country);
         series.push(totalMedals);
+        ids.push(country.id);
       });
+
+      this.numberJOs = labels.length;
+      labels = [...new Set(labels)];
+      this.numberCountries = labels.length;
 
       this.chartOptions = {
         series,
         labels,
         chart: {
-          type: "pie",
-          height: 350,
+          type: 'pie',
+          height: 400,
           events: {
             dataPointSelection: (event, chartContext, config) => {
-              const countryName = labels[config.dataPointIndex];
-              this.goToCountry(countryName);
-            }
-          }
+              const countryID = ids[config.dataPointIndex];
+              this.goToCountry(countryID);
+            },
+          },
         },
-        title: { text: "Répartition des médailles par pays" },
+        title: { text: '' },
         tooltip: {
           y: {
-            formatter: (val: number, opts?: any) =>
-              `${labels[opts.dataPointIndex]}: ${val} médailles`
-          }
+            formatter: (val: number, opts?: any) => `${val} medals`,
+          },
         },
       };
     });
   }
 
-  goToCountry(name: string) {
-    this.router.navigate(['/country', name]);
+  goToCountry(id: Number) {
+    this.router.navigate(['/country', id]);
   }
 }
