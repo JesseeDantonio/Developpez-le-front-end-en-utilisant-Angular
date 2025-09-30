b87b65f5911e37202154f466773a63fb0aaed978
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-09-30 07:55:24 +0200

    fix(type): suppression d'une propriété

--- src/app/core/types/PieChartOptions.ts
+++ src/app/core/types/PieChartOptions.ts
@@ -1,9 +1,10 @@
-import { ApexNonAxisChartSeries, ApexChart, ApexResponsive, ApexTitleSubtitle, ApexTooltip } from 'ng-apexcharts';
+import { ApexNonAxisChartSeries, ApexChart, ApexResponsive, ApexTitleSubtitle, ApexTooltip, ApexXAxis } from 'ng-apexcharts';
 
 export type PieChartOptions = {
   series: ApexNonAxisChartSeries;
   chart: ApexChart;
   labels: string[];
+  xaxis: any,
   responsive?: ApexResponsive[];
   title: ApexTitleSubtitle;
   tooltip: ApexTooltip;
